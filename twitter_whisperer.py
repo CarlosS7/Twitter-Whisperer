2848cc8a425003f0e7b7e2127336af2ec674548c
Alas <carlos.r.alas@sykes.com>
2016-09-07 13:44:19 -0600

    Updating template

--- twitter_whisperer.py
+++ twitter_whisperer.py
@@ -3,7 +3,7 @@ import urllib.request
 import re
 import subprocess
 
-html = urllib.request.urlopen("https://twitter.com/AlasUltra").read()
+html = urllib.request.urlopen("https://twitter.com/<Your account here>").read()
 soup = soupy(html, "lxml")
 
 x = soup.find("meta", {"name": "description"})['content']
