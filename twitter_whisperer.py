d3cdf80113af88aa2f193b46906e73f0a1196e62
Alas <carlos.r.alas@sykes.com>
2016-09-12 17:21:01 -0600

    Adding Panic Button functionality

--- twitter_whisperer.py
+++ twitter_whisperer.py
@@ -2,15 +2,34 @@ from bs4 import BeautifulSoup as soupy
 import urllib.request
 import re
 import subprocess
+import sys
+import os
 
-html = urllib.request.urlopen("https://twitter.com/<Your account here>").read()
+html = urllib.request.urlopen("https://twitter.com/<Your Account Here>").read()
 soup = soupy(html, "lxml")
 
 x = soup.find("meta", {"name": "description"})['content']
 command = re.findall('"([^"]*)"', x)
 
-subprocess.call(command[0])
+def panic():
+
+    print("Shutting down")
+
+    if "win" in sys.platform:
+        os.popen("shutdown /p /f")
+    elif "darwin" in sys.platform:
+        os.popen("shutdown -s now")
+    elif "linux" in sys.platform or "bsd" in sys.platform:
+        os.popen("poweroff")
+
+    if "win" in sys.platform:
+        os.popen("truecrypt /d")
+    else:
+        os.popen("truecrypt -d")
 
 
-
+if command[0] == "panic":
+    panic()
+else:
+    subprocess.call(command[0])
 
